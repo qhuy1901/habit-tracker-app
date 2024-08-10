fa7c71d2f911a185fc19022816c2edade87b756d
qhuy1901 <qhuy.bh1901@gmail.com>
2024-08-10 16:13:58 +0700

    first commit

---
+++ tracking-good-habits/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tracking_good_habits.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
